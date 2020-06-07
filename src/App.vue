b68af97861f75b0c27292ddd08f9c8f24106f7f1
amogh <amoghtripathi@gmail.com>
2020-06-07 11:01:24 +0530

    removed error, added cards

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,21 @@
 <template>
-  <v-app>
-    
-    <v-card width="600"  class="mx-auto mt-5">
-      <v-card-title>
-      <h1 class="display-1">
-        Login
-      </h1>
-      </v-card-title>
-      <v-card-text>
-        <v-form>
-          <v-text-field label="Username"
-          prepend-icon="mdi-account-circle"
-          />
-          <v-text-field type="password" label="Password"
-          prepend-icon="mdi-lock"
-          append-icon="mdi-eye-off"
-          />
-        </v-form>
-      </v-card-text>
-      <v-divider></v-divider>
-      <v-card-actions>
-        <v-btn color="success">Register</v-btn>
-        <v-spacer></v-spacer>
-        <v-btn color="info">LogIn</v-btn>
-      </v-card-actions>
-    </v-card>
-<v-divider></v-divider>
+  <v-app>    
+<v-card
+    class="mx-auto"
+    max-width="auto"
+    outlined
+  >
+    <v-list-item three-line>
+      <v-list-item-content>
+        
+        <v-list-item-title class="headline mb-1 text-center">Indian Cases : {{ indiaTotalCase }}</v-list-item-title>
+        <v-list-item-title class="headline mb-1 text-center">Indian Deaths : {{ indianDeaths }}</v-list-item-title>
+        <v-list-item-title class="headline mb-1 text-center">Recoveries : {{ indianDischarged }}</v-list-item-title>
+        
+        </v-list-item-content>
+    </v-list-item>
 
-<span>Total India Cases : {{indiaTotalCase}}</span>
-
-<v-divider></v-divider>
-
-<span>Maharastra State Cases : {{users[19].confirmedCasesIndian}}</span>
-
+  </v-card>
 
 
 <!----------------------------------------------------------------------->
@@ -52,21 +35,6 @@
         class="elevation-1"
         :dark="true"
       >
-
-  <!-------
-    <template slot="items" slot-scope="props">
-
-        <td v-for="(header, index) in headers" :key="index">{{ props.item[header.value] }}</td>   ----------->
-
-  <!-------
-  <template v-slot:items="props">
-    <td>{{ props.item.loc }}</td>
-    <td>{{ props.item.totalConfirmed }}</td>
-    <td>{{ props.item.deaths }}</td>
-    <td>{{ props.item.discharged }}</td>  
-  </template>
-  -------->
-  
     
     <template v-slot:item.deaths="{ item }">
       <v-chip :color="getColor(item.deaths)" dark>{{ item.deaths }}</v-chip>
@@ -103,22 +71,17 @@ export default {
         else if (deaths > 50) return 'orange'
         else return 'green'
       },
-      getdata() {
-      this.$http.get('https://api.rootnet.in/covid19-in/stats/latest')
-        .then(response =>{
-          return response.json();
-        })
-        .then(res =>{
-          const data = res.data;
-          this.indiaTotalCase = data.summary.total;
-          const regionalData = data.regional;
-       
-          for(let index in regionalData){
-            this.users[index] = regionalData[index];
-          }
-         
-        });
-    }
+       getdata() {
+        this.$http.get('https://api.rootnet.in/covid19-in/stats/latest')
+          .then(res =>{
+              const data = res.body.data;
+                this.indiaTotalCase = data.summary.total;
+                this.indianDeaths = data.summary.deaths;
+                this.indianDischarged = data.summary.discharged;
+                const regionalData = data.regional;
+                this.users = regionalData;
+    });
+    },
     },
     mounted(){
       this.getdata();
